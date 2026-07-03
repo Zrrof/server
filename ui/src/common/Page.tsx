0df58da83cc04a42c4ec567c05fe0f63768e5e26
Zrrof <126815586+Zrrof@users.noreply.github.com>
2026-07-03 21:17:07 +0200

    Refactor version query in Page component
    Refactor version query to improve readability and handling of default value.

--- ui/src/common/Page.tsx
+++ ui/src/common/Page.tsx
@@ -108,9 +108,11 @@ export const Page: React.FC = ({children}) => {
     const [mobileOpen, setMobileOpen] = React.useState(false);
     const [userMenuOpen, setUserMenuOpen] = React.useState<null | HTMLElement>(null);
     const [logout] = useMutation<Logout>(gqlUser.Logout, {refetchQueries: [{query: gqlUser.CurrentUser}]});
-    const {data: {version = gqlVersion.VersionDefault.version} = gqlVersion.VersionDefault} = useQuery<Version>(
-        gqlVersion.Version
-    );
+    const versionQuery = useQuery<Version>(gqlVersion.Version);
+    
+    const version =
+        versionQuery.data?.version ??
+        gqlVersion.VersionDefault.version;
     const dashboardsQuery = useQuery<Dashboards>(gqlDashboard.Dashboards);
     const dashboards = (dashboardsQuery.data && dashboardsQuery.data.dashboards) || [];
 
